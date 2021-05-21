f138bb712668066d84cfb562c68fa1653111d60e
Sengolda <79252176+Sengolda@users.noreply.github.com>
2021-05-21 11:30:13 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,25 +13,26 @@ if version.endswith(('a', 'b', 'rc')):
     try:
         import subprocess
         p = subprocess.Popen(['git', 'rev-list', '--count', 'HEAD'],
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = p.communicate()
         if out:
             version += out.decode('utf-8').strip()
         p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'],
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = p.communicate()
         if out:
             version += '+g' + out.decode('utf-8').strip()
     except Exception:
         pass
 
-setup(name='discord-ext-menus',
-      author='Rapptz',
-      url='https://github.com/Rapptz/discord-ext-menus',
-      version=version,
-      packages=['discord.ext.menus'],
-      license='MIT',
-      description='An extension module to make reaction based menus with discord.py',
-      install_requires=['discord.py>=1.2.5'],
-      python_requires='>=3.5.3'
+setup(
+        name='discord-ext-menus',
+        author='Rapptz',
+        url='https://github.com/Rapptz/discord-ext-menus',
+        version=version,
+        packages=['discord.ext.menus'],
+        license='MIT',
+        description='An extension module to make reaction based menus with discord.py',
+        install_requires=['discord.py>=1.2.5'],
+        python_requires='>=3.5.3'
 )
